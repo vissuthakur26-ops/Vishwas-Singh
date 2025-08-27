ff4405b54498368e0300157af7176b9f61997803
vissuthakur26-ops <vissuthakur26@gmail.com>
2025-08-27 13:40:54 +0530

    Add files via upload

---
+++ loop2.java
@@ -0,0 +1,15 @@
+public class loop2 {
+    public static void main(String[]args){
+        int n=5;
+        if(n>0){
+            System.out.println("it is a positive number");
+            else if (n<0){
+                System.out.println("nuber is negative");
+                else(n==0){
+                    System.out.println("number is 0");
+                }
+            }
+        }
+    }
+    
+}
